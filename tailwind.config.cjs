54f20e2075ec4b4b863018376a7b0ddaea4d8d48
ardy1110 <ardia0908@gmail.com>
2022-09-30 10:26:42 +0700

    commit pertama assignment

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+// File to enable tailwindcss intellisense
+module.exports = {
+    theme: {
+      screens: {
+        'handphone': '320px',
+        // => @media (min-width: 320px) { ... }
+  
+        'laptop': '1080px',
+        // => @media (min-width: 1080px) { ... }
+      },
+    }
+  }
